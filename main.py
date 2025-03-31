7da58d0bfd8751eeef440968b6e772f3693fd323
hawkwolfe <41488388+hawkwolfe@users.noreply.github.com>
2025-03-31 17:28:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,4 @@ from src import engine
 
 #This file is the entry point to start the game
 if __name__ == '__main__':
-=======
-from src import engine
-
-#This file is the entry point to start the game
-if __name__ == '__main__':
   engine.run()
