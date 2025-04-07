42462ab9d4e95907c2cb6a96462d0941b642fb0a
hawkwolfe <hawkwolfe@gmail.com>
2025-04-07 11:33:21 -0500

    Created player and projectile classes and basic engine for gameplay

--- src/Player.py
+++ src/Player.py
@@ -1,1 +1,79 @@
-#Module for player classes
+import os
+import pygame
+from . import globals
+from .projectile import Projectile
+
+sprite_path = os.path.join(os.path.dirname(__file__), '../assets/art/spaceships/bgbattleship.png')
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.image.load(sprite_path).convert_alpha()
+        self.start_img = self.image
+        self.rect = self.image.get_rect(center=pos)
+        self.pos = pygame.math.Vector2(pos)
+        self.vel = pygame.math.Vector2(0, 0)
+        self.acceleration = 4
+        self.friction = 1
+        self.max_speed = 500
+        self.fire_delay = 700
+        self.last_shot = pygame.time.get_ticks()
+        self.sprite_rotation_offset = -90
+
+    def update(self, dt):
+        self.pos += self.vel * dt
+        self.rect.center = self.pos
+        self.rotate()
+        self.check_bounds()
+        self.reduce_velocity()
+
+    def rotate(self):
+        _, self.angle = (pygame.mouse.get_pos() - self.pos).as_polar()
+        self.image = pygame.transform.rotozoom(
+            self.start_img, -self.angle + self.sprite_rotation_offset, 1
+        )
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def move(self, direction):
+        match direction:
+            case "UP":
+                if -1 * self.vel.y < self.max_speed:
+                    self.vel.y -= self.acceleration
+            case "DOWN":
+                if self.vel.y < self.max_speed:
+                    self.vel.y += self.acceleration
+            case "LEFT":
+                if -1 * self.vel.x < self.max_speed:
+                    self.vel.x -= self.acceleration
+            case "RIGHT":
+                if self.vel.x < self.max_speed:
+                    self.vel.x += self.acceleration
+
+    def shoot(self, group):
+        if pygame.time.get_ticks() - self.last_shot >= self.fire_delay:
+            self.last_shot = pygame.time.get_ticks()
+            origin = self.pos + pygame.math.Vector2.from_polar(
+                (self.image.get_height() / 2, self.angle)
+            )
+            projectile = Projectile(origin, self.angle)
+            group.add(projectile)
+
+    def reduce_velocity(self):
+        if self.vel.x < 0:
+            self.vel.x += self.friction
+        if self.vel.x > 0:
+            self.vel.x -= self.friction
+        if self.vel.y < 0:
+            self.vel.y += self.friction
+        if self.vel.y > 0:
+            self.vel.y -= self.friction
+
+    def check_bounds(self):
+        if self.pos.x < 0:
+            self.pos.x = globals.WINDOW_WIDTH
+        if self.pos.x > globals.WINDOW_WIDTH:
+            self.pos.x = 0
+        if self.pos.y < 0:
+            self.pos.y = globals.WINDOW_HEIGHT
+        if self.pos.y > globals.WINDOW_HEIGHT:
+            self.pos.y = 0
--- src/engine.py
+++ src/engine.py
@@ -1,6 +1,48 @@
+import os
 import pygame
-from . import Scene, UI, Player, Asteroid
+from . import globals
+from .player import Player
 
-#This method should run the game and is called in the top-level main module
+bg_path = os.path.join(os.path.dirname(__file__), '../assets/art/background.png')
+
+# This method runs the game and is called in the top-level main module
 def run():
-  print('Hello, World!')
+    pygame.init()
+    screen = pygame.display.set_mode((globals.WINDOW_WIDTH, globals.WINDOW_HEIGHT))
+    bg = pygame.image.load(bg_path)
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+    all_sprites = pygame.sprite.Group()
+    player = Player(pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2))
+    all_sprites.add(player)
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        all_sprites.update(dt)
+        screen.blit(bg, (0, 0))
+        all_sprites.draw(screen)
+
+        keys = pygame.key.get_pressed()
+        left_mouse, middle_mouse, right_mouse = pygame.mouse.get_pressed()
+        if keys[pygame.K_w]:
+            player.move("UP")
+        if keys[pygame.K_s]:
+            player.move("DOWN")
+        if keys[pygame.K_a]:
+            player.move("LEFT")
+        if keys[pygame.K_d]:
+            player.move("RIGHT")
+        if keys[pygame.K_SPACE] or left_mouse:
+            player.shoot(all_sprites)
+
+        pygame.display.flip()
+
+        # limits FPS and returns value for framerate-independent physics
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
