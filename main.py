d7bdcb64858fabe465e353cca1a55f048ddd6e0a
hawkwolfe <hawkwolfe@gmail.com>
2025-04-08 21:02:11 -0500

    Expanded comments

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import sys
 from src import engine
 
+"""Create temporary directory for build script."""
 if getattr(sys, "frozen", False):
     os.chdir(sys._MEIPASS)
 
-# This file is the entry point to start the game
+"""This file is the entry point to run the game engine."""
 if __name__ == "__main__":
     engine.run()
  
